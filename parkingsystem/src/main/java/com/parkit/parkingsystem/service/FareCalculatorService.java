66af6b6ac258a5edd9a8af2669649a3e6a6d5400
Bruno Mazel <46fcsbpm6869-student@anonymous-codio.com>
2023-05-05 14:24:36 +0000

    fix: resolved error in price calculation

--- parkingsystem/src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ parkingsystem/src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -10,11 +10,11 @@ public class FareCalculatorService {
             throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
         }
 
-        int inHour = ticket.getInTime().getHours();
-        int outHour = ticket.getOutTime().getHours();
+        int inHour = (int)ticket.getInTime().getTime();
+        int outHour = (int)ticket.getOutTime().getTime();
 
         //TODO: Some tests are failing here. Need to check if this logic is correct
-        int duration = outHour - inHour;
+        int duration = (outHour - inHour)/1000/60/60;
 
         switch (ticket.getParkingSpot().getParkingType()){
             case CAR: {
